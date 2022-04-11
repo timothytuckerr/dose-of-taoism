228cb927c466f446f766c93231096f955a421750
Timothy Tucker <timothytucker@berkeley.edu>
2022-04-11 15:21:14 -0700

    integrated with now working, updating api

--- extension/script.js
+++ extension/script.js
@@ -1,27 +1,11 @@
 window.onload = function(event) {
     console.log("load")
-    fetch('https://timoth-yt.herokuapp.com/daily-dose')
+    fetch('https://timoth-yt.herokuapp.com/api/taoism/daily-dose')
         .then(response => response.json())
         .then(data => {
             console.log(data)
             document.getElementById("header").innerHTML = "The Tao Te Ching, Chapter " + data['number']
             document.getElementById("text").innerHTML = data['text']
-    })
-    fetch('https://timoth-yt.herokuapp.com/daily-dose-image')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            document.body.style.backgroundImage = "url(" + data['url'] + ")";
+            document.body.style.backgroundImage = "url(" + data['image']['url'] + ")";
     })
 };
-
-let button = document.getElementById("refresh");
-button.addEventListener("click", function(event) {
-    console.log('click');
-    fetch('https://timoth-yt.herokuapp.com/refresh-daily-image')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            window.location.reload();
-    })
-})
